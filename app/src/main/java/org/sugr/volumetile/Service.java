35c388d2d0968f9f4f4ab0eb61dc8db78749989f
Viktor Kojouharov <developer@sugr.org>
2016-03-16 08:41:39 +0200

    Initial commit

---
+++ app/src/main/java/org/sugr/volumetile/Service.java
@@ -0,0 +1,8 @@
+package org.sugr.volumetile;
+
+public class Service extends android.service.quicksettings.TileService {
+    @Override
+    public void onClick() {
+        super.onClick();
+    }
+}
